21768a1a5ed5141f42e7085593962266b69037c4
Alex Wong de Freitas <otaru1921@gmail.com>
2021-11-19 22:36:55 -0300

    - Added update support on the ReportController.

--- backend/src/main/java/com/aps/webapp/controller/ReportController.java
+++ backend/src/main/java/com/aps/webapp/controller/ReportController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,6 +71,13 @@ public class ReportController {
 			return reportResponse;
 		}	
 		else return null;
+	}
+
+	@PutMapping(value="/{id}")	
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public Report updateReport(@PathVariable Long id, @RequestBody Report report) {
+		report.setId(id);
+		return reportRepository.save(report);
 	}
 
 	@PostMapping
